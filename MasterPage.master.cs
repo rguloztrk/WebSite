8f75a0617bd988f0b5a414161ad4deff2e48d7a2
Rukiye Ozturk <33896049+rguloztrk@users.noreply.github.com>
2019-01-03 19:44:14 +0300

    Add files via upload

---
+++ MasterPage.master.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+public partial class MasterPage : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DuyurulariGetir();
+    }
+
+    private void DuyurulariGetir()
+    {
+
+
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+
+        string sorgu = "Select * from Duyurular order by Tarih";
+
+
+        SqlCommand cmd = new SqlCommand(sorgu, con);
+        con.Open();
+
+        SqlDataReader dr = cmd.ExecuteReader();
+
+        lstDuyuru.DataSource = dr;
+        lstDuyuru.DataBind();
+
+        con.Close();
+    }
+
+    protected void btnKayit_Click(object sender, EventArgs e)
+    {
+        if (txtKullaniciAdi.Text != "" && txtSifre.Text != "")
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+
+            string sorgu = "Insert into Kullanicilar (KullaniciAdi, Sifre) Values (@kullaniciadi, @sifre)";
+
+
+            SqlCommand cmd = new SqlCommand(sorgu, con);
+            con.Open();
+            try
+            {
+                cmd.Parameters.AddWithValue("@kullaniciadi", txtKullaniciAdi.Text);
+                cmd.Parameters.AddWithValue("@sifre", txtSifre.Text);
+
+                cmd.ExecuteNonQuery();
+
+                con.Close();
+
+                lblSonuc.Text = "Basariyla kayit yapilmistir";
+
+            }
+            catch (Exception)
+            {
+
+                lblSonuc.Text = "Kaydiniz yapilamamistir";
+            }
+
+        }
+
+        else
+        {
+            lblSonuc.Text = "Bos alanlari doldurmaniz gerekir!!";
+
+        }
+
+        txtKullaniciAdi.Text = "";
+        txtSifre.Text = "";        
+
+
+    }
+}
